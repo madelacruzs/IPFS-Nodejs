dbffa63e97d329f168e9aeaefcfc7579a9dc37d6
Miguel De La Cruz <iscrusman@gmail.com>
2020-05-12 09:05:23 -0700

    remove
    remove

--- config.js
+++ config.js
@@ -1,16 +1,16 @@
 module.exports = {
-    MONGODB: "mongodb://jwt:@.mlab.com:37488/jwt-privasee",
-    jwt: {
-        secret_key: "12345qwerty"
-    },
-    PORT: 10001,
-    MSSQL: {
-        user: 'XXX',
-        password: 'XXX',
-        server: '.XXX', 
-        database: 'XXX',
-        options: {
-            encrypt: true // Use this if you're on Windows Azure
-        }
-    }
-}
+	MONGODB: "",
+	jwt: {
+		secret_key: "",
+	},
+	PORT: 10001,
+	MSSQL: {
+		user: "",
+		password: "",
+		server: "",
+		database: "",
+		options: {
+			encrypt: true, // Use this if you're on Windows Azure
+		},
+	},
+};
